95e4de13ac0708913736af1cd9394cbf11acc36b
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-05-11 15:35:56 +0300

    refactor: set correct output string

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,23 +3,19 @@ const fs = require('fs');
 
 const folder = path.join(__dirname, 'secret-folder');
 
-
 fs.readdir(folder, {withFileTypes: true},
   (error, files) => {
 
     if (error) throw error;
 
     for (const file of files) {
-
-      if (file.isFile() === true) {
-
-        const fileName = path.parse(file.name).name;
+      if (file.isFile()) {
+        const { name } = path.parse(file.name);
         const ext = path.extname(file.name);
 
         fs.stat(`${folder}/${file.name}`, (error, stats) => {
-          
           if (error) throw error;
-          console.table(`<${fileName}> - <${ext}> - <${stats.size} bytes>`);
+          console.table(`${name} - ${ext} - ${stats.size} byte`);
         });
       }
     }
